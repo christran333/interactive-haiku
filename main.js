bdeec55c6fc19fd6dd022916b885a687af10041c
christran333 <christophertran2005@gmail.com>
2026-04-07 17:58:16 +0000

    HUGE BIG BODY COMMIT THIS WILL END IT ALL

--- main.js
+++ main.js
@@ -2,18 +2,28 @@ function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-let divs = document.querySelectorAll(".grid-container span");
+let spans = document.querySelectorAll(".grid-container span");
+let lines = document.querySelectorAll(".grid-container .line");
 
 function randomize() {
-    divs.forEach(function (div) {
+    lines.forEach(function (line) {
+        let translateX = randomNumber(-30, 30);
+        let translateY = randomNumber(-20, 20);
+        let rotate = randomNumber(-15, 15);
+        line.style.transform =
+            `translate(${translateX}%, ${translateY}%) rotate(${rotate}deg)`;
+    });
+
+    spans.forEach(function (span) {
         let scale = randomNumber(3, 15) / 10;
         let translateX = randomNumber(-200, 200);
         let translateY = randomNumber(-200, 200);
         let rotate = randomNumber(0, 360);
-        div.style.transform =
+        span.style.transform =
             `scale(${scale}) translate(${translateX}%, ${translateY}%) rotate(${rotate}deg)`;
     });
-    console.log(divs.length, "randomize!");
+
+    console.log("randomize!");
 }
 
 document.addEventListener("click", randomize);
