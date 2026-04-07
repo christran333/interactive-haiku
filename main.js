491cb9b19e548044f3058fa94961773a9517e3ff
christran333 <christophertran2005@gmail.com>
2026-04-07 17:30:55 +0000

    yo

---
+++ main.js
@@ -0,0 +1,19 @@
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let divs = document.querySelectorAll(".grid-container div");
+
+function randomize() {
+    divs.forEach(function (div) {
+        let scale = randomNumber(5, 12) / 10;
+        let translateX = randomNumber(-50, 50);
+        let translateY = randomNumber(-30, 30);
+        let rotate = randomNumber(0, 360);
+        div.style.transform =
+            `scale(${scale}) translate(${translateX}%, ${translateY}%) rotate(${rotate}deg)`;
+    });
+    console.log(divs.length, "randomize!");
+}
+
+document.addEventListener("click", randomize);
